559e13a5295f0eff3ef95bb95c75cd3f06abecac
marcosdcl <marcosdouglasclima@gmail.com>
2020-12-28 12:58:58 -0300

    add read files

--- metodos.py
+++ metodos.py
@@ -1,26 +1,40 @@
-'''
-Para esta atividade você deve criar as seguintes funcionalidades:
-_ listagem de marketplaces (lista fixa). -> ok
-_ listagem de categorias por marketplace (lista fixa). -> ok
-_ listagem de subcategorias por categoria (lista fixa). -> ok
-_ ser possível acessar os métodos de listagem tanto pela web quando pelo console
-
-Para esta atividade você deve atender aos seguintes requisitos:
-_ ser escrito utilizando o framework Flask.
-_ separar o código da camada visual(flask e console) da camada de negócio(métodos de listagem).
-_ ser persistido apenas em memória.
-_ ser utilizado GITHUB.
-_ entregue ate 18:00 22/12/2020
-'''
 
 def marketplaces():
-    marketplace_list = ['MeLi', 'B2W', 'Magalu']
+    title = 'marketplaces'
+    marketplace_list = read_files(title)
     return marketplace_list
 
+
 def categories():
-    category_list = ['Móveis', 'Papelaria', 'Música', sub_categories()]
+    title = 'categories'
+    category_list = read_files(title)
+    category_list.append(sub_categories())
     return category_list
 
+
 def sub_categories():
-    sub_category_list = ['Cozinha', 'Escritório', 'Percursão']
+    title = 'sub_categories'
+    sub_category_list = read_files(title)
     return sub_category_list
+
+
+def read_files(title):
+    file = open('lists.txt', 'r')
+
+    for line in file:
+        list_ = line.strip().split(';')
+
+        if list_[0] == title:
+            list_.remove(title)
+            file.close()
+            return list_
+
+        elif list_[0] == title:
+            list_.remove(title)
+            file.close()
+            return list_
+
+        elif list_[0] == title:
+            list_.remove(title)
+            file.close()
+            return list_
